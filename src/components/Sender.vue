df7712ab8073c3ff6e08740376dc0a7f8f5a507b
Lucas Passos <lucas.depassos@yahoo.com.br>
2020-09-15 15:02:21 -0300

    mail test

--- src/components/Sender.vue
+++ src/components/Sender.vue
@@ -3,7 +3,8 @@
     <v-form color="primary">
 
     </v-form>
-    <v-btn v-model="callAPI" @click="callAPI">Chamada da API</v-btn>
+    <v-btn v-model="callAPI" @click="callAPI">Puxar Listas</v-btn>
+    <v-btn v-model="callAPI" @click="callAPI">Puxar Contatos da Lista 1</v-btn>
 </div>
 </template>
 
@@ -15,11 +16,12 @@ export default {
 
         callAPI() {
             const url = new URL(
-                "https://api-app.iporto.com.br/api/panel/addressbook/5016/contacts"
+                "https://api-app.iporto.com.br/api/panel/smtp/1/account/1/email/send/test"
             );
 
             let params = {
-                //,
+                "domain": "1617",
+                "account": "2204",
             };
             Object.keys(params)
                 .forEach(key => url.searchParams.append(key, params[key]));
@@ -27,12 +29,12 @@ export default {
             let headers = {
                 "Content-Type": "application/json",
                 "Accept": "application/json",
-                "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImVkMTY1YjNjNjJjYTgyOWNlNzRlOGMzOThjYjY5YTgxMGUxYmY3MTA2NzY0MzdkMmIwZTMyZTU1YjM0NDc4ZGQ5ZThiMTU4MTI2NzY2NzQ3In0.eyJhdWQiOiIxIiwianRpIjoiZWQxNjViM2M2MmNhODI5Y2U3NGU4YzM5OGNiNjlhODEwZTFiZjcxMDY3NjQzN2QyYjBlMzJlNTViMzQ0NzhkZDllOGIxNTgxMjY3NjY3NDciLCJpYXQiOjE2MDAxMDU1NzcsIm5iZiI6MTYwMDEwNTU3NywiZXhwIjoxNjMxNjQxNTc3LCJzdWIiOiIxOTUyIiwic2NvcGVzIjpbXX0.uqi4TNsLaWHzfS8ysZNkv4afNgCuqXloxnMVQH0H1GQmRxgXsdwILSZ37ttNPn6vzbRk25gNtSbLnuxDd4oXVN94uMnBwrk-Fx4Pi8Q0rNrhmO47broTwEmJF4fSFCiRn7JcKt4GyoWeSDU-cnAZi2STKEA6Ag79EhB_0q3hwlmHuGQIKdoJVu30fM76IUWJtdqpksdDZJ9AX0_m08R0X15mA323E9L4HzER6jWOz00octsYgq4OkTWvWkKoWBYyOkLkjYgB4Pfb4NWwGjAIvVzXj1uAa14w8q8OGspibgUzgGTdqdYfn1hjBH0FCGCa38xqnRI2a0hTWmVeJK5uj2TBgDmIgmV7622ObNfe_TC0i4vRbFwip--1kNZv6F8s5RTXowHfMQXzW4_LjsIUPteNnQIyDSjckVvBbdk41TEfqr6AWK52XdiSpX5eqzrlsP8X7plq71IeZDtB35tc-IqUMgSCvwhfyul5ys5MEb_mP6i7YPxVU6JYCvz7pgbz_Qi8TU67XRRk4LdXh-m178gMp3j4em_QpeBpk6hc-jdIg0sh6nN_ARXjteUePnY_avdMjCh9hrxRQSkwMWUjJGLIt0b9-No8r6QkKlZkggN0ScQe3BLLKsLfnwMXh6k_o4B8ocdqxeYKFpBxXNMLmOTDrieXleeSivLL-D2P6vw"
+                "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQ5YzQwNTNmNmYxMTMwNzM0M2RmYjI2N2E0NjRiMGZlNGNmMmQwMWM4YTBjMjM3MjIzYmUwMGYxZjk4NjgxM2NmOWMwNTU1ZGY2NDY0OTE5In0.eyJhdWQiOiIxIiwianRpIjoiZDljNDA1M2Y2ZjExMzA3MzQzZGZiMjY3YTQ2NGIwZmU0Y2YyZDAxYzhhMGMyMzcyMjNiZTAwZjFmOTg2ODEzY2Y5YzA1NTVkZjY0NjQ5MTkiLCJpYXQiOjE2MDAxNzk4MjUsIm5iZiI6MTYwMDE3OTgyNSwiZXhwIjoxNjMxNzE1ODI1LCJzdWIiOiIxOTQyIiwic2NvcGVzIjpbXX0.PRLWPbEOoXe6Al6nm2u_DY7uM03wxG1V2Efe36dlZJvL_izlszSIhzNQWCuDmbktoPP7cY5CLcZzbwtYmZ3wVwyznk6W1Ll-Qf5vNFFqHY09gymv88aQ2nX3RAKy_kcmXuo4PzxqJ7mdHA14cJZiAaU8bHCKZz0pqvUa6ftorBaY7TQXf1V5vQifhuiUPRQaeu438lF89mHvObBcfkOjDIsJulePtnNxxfOuh5_27L4Jvu86zgCUji5DvSt9C25IJ6FXX6kWkhVqLw8mkWW5Ga2UG1-CHbSMSb2GQGKcuFXjMkK6PwSo5BlLm7Bar3YHn6cW6J1yyqYtBvaYX9RK2c5t3qeNcF8DoXXKBu5Qy6knIcdbTlqgN9zfwZ-gs6Lg0H5XYUWamuTahGuMrgs7Q2kuw7QzBaqfOLCEFdxbN5viIriKxe-PUA9RTH29KiIawm6TWylYlcCKdMtq4krOwFJZJw0bbwiXjb7TIiUn03xYcXepF3Gy0u_X9M5MbI_DhySZaPcXUVdCPgBXNdU_EwLyk3ajBBsNfQuVpD3q4v1Dl8MB9aNe-8owaj8N7VE_stVacEN-TR7XeRLd7Q_PmLC03DBwbRrGF0J7HIoa5GhJph5c1E04OmHhAT_XrqKhSsYIIYqzWg_8kNoFIeii34Gu6jmnliygx43oYOmjdbc"
 
             };
 
             fetch(url, {
-                    method: "GET",
+                    method: "POST",
                     headers: headers,
                 })
                 .then(response => response.json())
